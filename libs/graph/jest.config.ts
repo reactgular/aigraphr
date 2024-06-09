173cdd73d70fb0880bc550c09bf023f29f34fde4
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-06-09 07:34:58 -0500

    Add new graph and core plugins (#6)
    This pull request adds two new plugins, "graph" and "core", to the
    project. The "graph" plugin includes initial setup and configuration for
    a new graph library, while the "core" plugin includes initial setup for
    a core plugin using Nx, Jest, and NestJS. The plugins come with
    TypeScript configuration files and necessary dependencies. This PR also
    includes updates to package.json and project.json files.

--- libs/graph/jest.config.ts
+++ libs/graph/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'nodes',
+  displayName: 'graph',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/plugins/nodes'
+  coverageDirectory: '../../coverage/libs/graph'
 };
