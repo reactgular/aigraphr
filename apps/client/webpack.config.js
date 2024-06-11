87efa92a5f979f5dc5cd6a138cb9bd98167da1a2
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-06-11 07:58:03 -0500

    9 host the static client when running server in production (#10)
    - feat(bootstrap.ts): add static assets support for serving client app
    refactor(bootstrap.ts): use NestExpressApplication for enhanced express
    features
    - chore(project.json): add implicit dependency on client to ensure
    proper build order
    - refactor(client): change output path in webpack config to align with
    server structure refactor(server): simplify static assets path in
    bootstrap for better maintainability fix(server): update webpack config
    to include client assets for consistent build output

--- apps/client/webpack.config.js
+++ apps/client/webpack.config.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/client')
+    path: join(__dirname, '../server/src/client')
   },
   devServer: {
     port: 4200
--- apps/server/src/bootstrap.ts
+++ apps/server/src/bootstrap.ts
@@ -1,9 +1,15 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
 
 export const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets('client');
+
+  // app.setBaseViewsDir('views');
+  // app.setViewEngine('hbs');
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
