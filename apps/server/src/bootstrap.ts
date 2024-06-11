bed25c5ed428a97687c5030b915563eec250a094
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-06-11 07:23:50 -0400

    feat(bootstrap.ts): add static assets support for serving client app refactor(bootstrap.ts): use NestExpressApplication for enhanced express features

--- apps/server/src/bootstrap.ts
+++ apps/server/src/bootstrap.ts
@@ -1,9 +1,18 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app/app.module';
 
 export const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // @note: debug only, not prod ready
+  app.useStaticAssets(join(__dirname, '..', '..', '..', 'dist', 'apps', 'client'));
+
+  // @note: this adds support for controller views using handlebars templates in views folder
+  // app.setBaseViewsDir('views');
+  // app.setViewEngine('hbs');
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
