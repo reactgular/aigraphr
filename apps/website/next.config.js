41e849f8fb30c5a9f9f6487e35a7df78a5249fa5
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-06-11 09:17:55 -0400

    feat(website): configure custom output directory and export mode in next.config.js refactor(website): remove unused hello API route to clean up codebase

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -7,6 +7,8 @@ const { composePlugins, withNx } = require('@nx/next');
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  output: "export",
+  distDir: "../../dist/apps/website",
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
