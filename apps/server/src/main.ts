51175c4e05524036bed5f9dd19b9f34d78f67381
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-06-14 10:03:15 -0400

    fix(main.ts): correct spelling mistake in brand name concatenation

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,11 +5,8 @@ import { getBrandConfig } from './config/brand.config';
 
 const brand = getBrandConfig();
 
-// Adding a fix for a fake bug to test release
-const x = 123;
-
 cli({
-  name: brand.name,
+  name: `${brand.name}s`, // fix: spelling mistake
   version: brand.version,
   help: { version: brand.version, description: brand.description },
   commands: [start],
