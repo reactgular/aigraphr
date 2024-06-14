28dd2aa1059e14625edaa77a7fa34ee02992580e
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-06-14 10:02:39 -0400

    chore(main.ts): add temporary variable for testing release process

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,9 @@ import { getBrandConfig } from './config/brand.config';
 
 const brand = getBrandConfig();
 
+// Adding a fix for a fake bug to test release
+const x = 123;
+
 cli({
   name: brand.name,
   version: brand.version,
