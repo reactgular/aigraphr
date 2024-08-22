f7ed24ce863c1f2f3adc89a5d553b80e0af9070d
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-08-22 07:32:20 -0400

    feat(page.tsx): replace Welcome and ColorSchemeToggle components with FaqSimple to enhance user experience feat(FaqSimple): add FaqSimple component with accordion for frequently asked questions style(FaqSimple.module.css): create styles for FaqSimple component to improve layout and design
    chore(Welcome.tsx): remove Welcome component as it is no longer needed in the project

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,16 +1,14 @@
 import {FC} from 'react';
-import {ColorSchemeToggle} from '../src/components/ColorSchemeToggle';
+import {FaqSimple} from '../src/components/FaqSimple';
 import {FeaturesCards} from '../src/components/FeaturesCards';
 import {HeroImageRight} from '../src/components/HeroImageRight';
-import {Welcome} from '../src/components/Welcome';
 
 const Index: FC = () => {
     return (
         <>
             <HeroImageRight />
             <FeaturesCards />
-            <Welcome />
-            <ColorSchemeToggle />
+            <FaqSimple />
         </>
     );
 };
