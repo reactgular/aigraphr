75bb6d8e308a977e186846cb05cbc035138adc02
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-11-21 09:19:34 -0500

    chore: update ESLint configuration and dependencies for improved code quality and consistency across projects
    - Remove redundant tab size option from Project.xml.
    - Add a new ESLint configuration file for the root project and server.
    - Update client ESLint configuration to extend the new root configuration.
    - Add ESLint and TypeScript-related dependencies to package.json for better linting support.
    - Modify the App component to use a more concise state update function.
    - Remove the outdated ESLint configuration file from the server package.

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -3,9 +3,11 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import config from '../../eslint.config.mjs';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', '**/*.d.ts'] },
+  config,
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
