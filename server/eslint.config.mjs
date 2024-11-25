e59286a04aff0f6337a0c386115b74301db55186
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-11-25 07:53:01 -0500

    chore: update code style configurations and integrate Prettier for consistent formatting
    This commit modifies various configuration files to standardize code
    style across the project. It introduces Prettier for automatic code
    formatting, updates ESLint configurations to align with Prettier,
    and adjusts indentation settings to use 4 spaces. These changes aim
    to enhance code readability and maintainability.

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,19 +1,15 @@
-import ts_eslint from 'typescript-eslint';
-import config from '../eslint.config.mjs';
+import ts_eslint from "typescript-eslint";
+import rootConfig from "../eslint.config.mjs";
 
-export default ts_eslint.config(
-    { ignores: ['dist'] },
-    config,
-    {
-        files: ['**/*.ts'],
-        languageOptions: {
-            ecmaVersion: 2020
-        },
-        rules: {
-            '@typescript-eslint/interface-name-prefix': 'off',
-            '@typescript-eslint/explicit-function-return-type': 'off',
-            '@typescript-eslint/explicit-module-boundary-types': 'off',
-            '@typescript-eslint/no-explicit-any': 'off'
-        }
-    }
-);
+export default ts_eslint.config({ ignores: ["dist"] }, rootConfig, {
+    files: ["**/*.ts"],
+    languageOptions: {
+        ecmaVersion: 2020,
+    },
+    rules: {
+        "@typescript-eslint/interface-name-prefix": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+    },
+});
