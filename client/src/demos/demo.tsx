434ac2dd4e293eab6e534cf314987d19c0e1a835
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-03 08:17:26 -0500

    feat(AppProviders): create AppProviders component to wrap the app with React Query context refactor(demo): integrate AppProviders into Demo component for improved state management with React Query

--- client/src/demos/demo.tsx
+++ client/src/demos/demo.tsx
@@ -1,5 +1,6 @@
 import {AppHeader} from '@/components/app/AppHeader.tsx';
 import {AppPlaceholder} from '@/components/app/AppPlaceholder.tsx';
+import {AppProviders} from '@/components/app/AppProviders.tsx';
 import {AppSidebar} from '@/components/app/AppSidebar.tsx';
 import {
     SidebarInset,
@@ -12,14 +13,16 @@ import {FC} from 'react';
  */
 export const Demo: FC = () => {
     return (
-        <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-                <AppHeader />
-                <main className="flex flex-1 flex-col gap-4 p-4">
-                    <AppPlaceholder />
-                </main>
-            </SidebarInset>
-        </SidebarProvider>
+        <AppProviders>
+            <SidebarProvider>
+                <AppSidebar />
+                <SidebarInset>
+                    <AppHeader />
+                    <main className="flex flex-1 flex-col gap-4 p-4">
+                        <AppPlaceholder />
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
+        </AppProviders>
     );
 };
