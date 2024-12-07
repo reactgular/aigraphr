f661ef429d980163e98cda7ecc27424305d5382b
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-07 07:20:32 -0500

    chore: remove TRPC dependencies and related code from client and server to simplify the project structure and reduce complexity

--- client/src/components/app/AppProviders.tsx
+++ client/src/components/app/AppProviders.tsx
@@ -1,18 +1,13 @@
 import {useQueryClient} from '@/components/hooks/useQueryClient';
-import {useTrpcClient} from '@/components/hooks/useTrpcClient';
-import {trpc} from '@/trpc';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {FC, PropsWithChildren} from 'react';
 
 export const AppProviders: FC<PropsWithChildren> = ({children}) => {
     const queryClient = useQueryClient();
-    const trpcClient = useTrpcClient();
 
     return (
-        <trpc.Provider queryClient={queryClient} client={trpcClient}>
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
-        </trpc.Provider>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
     );
 };
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,10 @@
 import {Module} from '@nestjs/common';
-import {TRPCModule} from 'nestjs-trpc';
 import {AppController} from './app.controller';
-import {AppRouter} from './app.router';
 import {AppService} from './app.service';
-import {TrpcPanelController} from './trpc-panel.controller';
 
 @Module({
-    imports: [
-        TRPCModule.forRoot({
-            autoSchemaFile: '../shared/src/trpc'
-        })
-    ],
-    controllers: [AppController, TrpcPanelController],
-    providers: [AppService, AppRouter]
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
