714f0e4a8d4cd1ed158e868b4c0d8582c68a0b77
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-11 07:50:32 -0500

    chore(deprecated): remove deprecated project-related DTOs, models, services, and controllers to clean up the codebase and improve maintainability

--- server/src/projects/_deprecated/dtos/project-create.dto.ts
+++
@@ -1,7 +0,0 @@
-import {ProjectDto} from '@/projects/_deprecated/dtos/project.dto';
-import {PickType} from '@nestjs/swagger';
-
-/**
- * @deprecated
- */
-export class ProjectCreateDto extends PickType(ProjectDto, ['name'] as const) {}
