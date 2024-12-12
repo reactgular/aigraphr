17f5c308f674a375c0353c279828dca36944b6cd
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-12 12:14:32 -0500

    feat(projects): enhance createProjectCrudController to include projectId in parameters for better project management refactor(workspaces): replace createCrudController with createProjectCrudController to utilize new projectId parameter structure

--- server/src/projects/controllers/create-project-crud.controller.ts
+++ server/src/projects/controllers/create-project-crud.controller.ts
@@ -1,14 +1,30 @@
+import {ProjectIdDto} from '@/projects/dtos/project-id.dto';
 import {
     createCrudController,
     ScaffoldCrudOptions
 } from '@/scaffold/controllers/create-crud.controller';
-import {ScaffoldEmptyDto} from '@/scaffold/dtos/scaffold-empty';
 import {ScaffoldEntity} from '@/scaffold/services/scaffold-entity.service';
 
 export function createProjectCrudController<
     TDto extends ScaffoldEntity,
-    TEntity extends ScaffoldEntity,
-    TParamDto extends ScaffoldEmptyDto
->(options: ScaffoldCrudOptions<TDto, TParamDto>) {
-    return createCrudController<TDto, TEntity, TParamDto>(options);
+    TEntity extends ScaffoldEntity
+>(options: Omit<ScaffoldCrudOptions<TDto, ProjectIdDto>, 'getParams'>) {
+    return createCrudController<TDto, TEntity, ProjectIdDto>({
+        ...options,
+        getParams: {
+            params: [
+                {
+                    name: 'id',
+                    type: Number,
+                    required: true
+                },
+                {
+                    name: 'projectId',
+                    type: Number,
+                    required: true
+                }
+            ],
+            dto: ProjectIdDto
+        }
+    });
 }
--- server/src/workspaces/controllers/workspaces.controller.ts
+++ server/src/workspaces/controllers/workspaces.controller.ts
@@ -1,11 +1,10 @@
-import {ProjectIdDto} from '@/projects/dtos/project-id.dto';
+import {createProjectCrudController} from '@/projects/controllers/create-project-crud.controller';
 import {
     WorkspaceCreateDto,
     WorkspaceDto,
     WorkspaceUpdateDto
 } from '@/projects/entities/workspace.entity';
 import {ProjectGuard} from '@/projects/gaurds/project.guard';
-import {createCrudController} from '@/scaffold/controllers/create-crud.controller';
 import {ScaffoldCrudService} from '@/scaffold/services/scaffold-crud.service';
 import {WorkspacesService} from '@/workspaces/services/workspaces.service';
 import {Controller, UseGuards} from '@nestjs/common';
@@ -14,23 +13,8 @@ import {ApiTags} from '@nestjs/swagger';
 @ApiTags('Workspaces')
 @Controller('projects/:projectId/workspaces')
 @UseGuards(ProjectGuard)
-export class WorkspacesController extends createCrudController({
+export class WorkspacesController extends createProjectCrudController({
     getDto: WorkspaceDto,
-    getParams: {
-        params: [
-            {
-                name: 'id',
-                type: Number,
-                required: true
-            },
-            {
-                name: 'projectId',
-                type: Number,
-                required: true
-            }
-        ],
-        dto: ProjectIdDto
-    },
     createDto: WorkspaceCreateDto,
     updateDto: WorkspaceUpdateDto
 }) {
