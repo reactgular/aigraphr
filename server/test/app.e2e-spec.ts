373480b08d75c543b3f08d16d77e100b0097ed19
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-16 08:05:00 -0500

    refactor(tests): rename AppController tests to ProjectsController and update imports for better clarity and organization feat(tests): enhance test setup by enabling CORS, global pipes, and filters, and set a global prefix for API routes

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,24 +1,40 @@
-import {AppModule} from '@/app/app.module';
+import {ProjectDto} from '@/entities/project.entity';
+import {MainModule} from '@/main.module';
+import {ScaExceptionFilter} from '@/scaffold/filters/sca-exception.filter';
+import {scaValidationPipe} from '@/scaffold/pipes/sca-validation.pipe';
 import {INestApplication} from '@nestjs/common';
+import {HttpAdapterHost} from '@nestjs/core';
 import {Test, TestingModule} from '@nestjs/testing';
 import request from 'supertest';
 
-describe('AppController (e2e)', () => {
+describe('ProjectsController (e2e)', () => {
     let app: INestApplication;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [AppModule]
+            imports: [MainModule]
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.enableCors();
+        app.useGlobalPipes(scaValidationPipe());
+        app.useGlobalFilters(new ScaExceptionFilter(app.get(HttpAdapterHost)));
+        app.enableShutdownHooks();
+        app.setGlobalPrefix('api');
+
         await app.init();
     });
 
     it('/ (GET)', () => {
         return request(app.getHttpServer())
-            .get('/')
+            .get('/api/projects')
             .expect(200)
-            .expect('Hello World!');
+            .expect([
+                {
+                    id: 1,
+                    name: 'project',
+                    open: false
+                } satisfies ProjectDto
+            ]);
     });
 });
