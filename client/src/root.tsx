f6b9693cda227f589f4da2038ea883777d4a6bf5
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-25 08:22:45 -0500

    chore(root.tsx): remove unused font preconnect and stylesheet links to clean up code and improve performance

--- client/src/root.tsx
+++ client/src/root.tsx
@@ -14,16 +14,6 @@ import '@fontsource/inter/400.css';
 import '@fontsource/inter/400-italic.css';
 
 export const links: Route.LinksFunction = () => [
-    {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
-    {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossOrigin: 'anonymous'
-    },
-    {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-    },
     {rel: 'stylesheet', href: stylesheet}
 ];
 
