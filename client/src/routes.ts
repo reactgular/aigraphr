b260c6d0919f3b9aa7fe8d0949bc6476157861ed
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-27 06:29:22 -0500

    feat(routes): add new workspace routes and components for project management feat(routes): enhance existing routes with comments for better clarity and navigation

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -8,11 +8,24 @@ import {
 
 export default [
     layout('routes/layout.tsx', [
+        // path: "/"
         index('routes/index.tsx'),
         ...prefix('projects', [
+            // path: "/projects"
             index('routes/projects/index.tsx'),
             layout('routes/projects/layout.tsx', [
-                route(':projectId/dashboard', 'routes/projects/dashboard.tsx')
+                // path: "/projects/:projectId/dashboard"
+                route(':projectId/dashboard', 'routes/projects/dashboard.tsx'),
+                // path: "/projects/:projectId/workspaces"
+                route(
+                    ':projectId/workspaces',
+                    'routes/projects/workspaces/index.tsx'
+                ),
+                // path: "/projects/:projectId/workspaces/:workspaceId"
+                route(
+                    ':projectId/workspaces/:workspaceId',
+                    'routes/projects/workspaces/workspace.tsx'
+                )
             ])
         ])
     ])
