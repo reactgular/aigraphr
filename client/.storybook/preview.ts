78497b85fc791b12263b0b259a3abe09c17084d0
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2024-12-31 07:35:08 -0500

    feat(storybook): add Inter font styles to Storybook for consistent typography fix(root.tsx): update import path for Route type to correct relative path

--- client/.storybook/preview.ts
+++ client/.storybook/preview.ts
@@ -1,4 +1,24 @@
 import type {Preview} from '@storybook/react';
+import '../src/styles.css';
+import '@fontsource/inter';
+import '@fontsource/inter/100.css';
+import '@fontsource/inter/100-italic.css';
+import '@fontsource/inter/200.css';
+import '@fontsource/inter/200-italic.css';
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/300-italic.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/400-italic.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/500-italic.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/600-italic.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/700-italic.css';
+import '@fontsource/inter/800.css';
+import '@fontsource/inter/800-italic.css';
+import '@fontsource/inter/900.css';
+import '@fontsource/inter/900-italic.css';
 
 const preview: Preview = {
     tags: ['autodocs'],
