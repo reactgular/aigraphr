1468cc1b7da19a399da98e70fea55b19455ae66e
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2025-01-05 08:37:17 -0500

    feat(GrNodeFooter): add Tally3 icon to enhance visual representation of footer

--- client/src/components/graph/GrNodeFooter.tsx
+++ client/src/components/graph/GrNodeFooter.tsx
@@ -1,9 +1,11 @@
 import {cn, type PropsWithClassName} from '@/components/shadcn/lib/utils';
+import {Tally3} from 'lucide-react';
 import type {FC} from 'react';
 
 export const GrNodeFooter: FC<PropsWithClassName> = ({className}) => {
     return (
         <div className={cn('rounded-b-md bg-gray-200/60', className)}>
+            <Tally3 className="rotate-90" />
             <div className="px-4 py-2 truncate overflow-hidden">Footer</div>
         </div>
     );
