237f16aa2429b4f4328f13bddd3675aa29aa561d
Nick Foscarini <50146659+codemile@users.noreply.github.com>
2025-01-06 07:33:01 -0500

    feat(graph): implement toggle group in GrNodeFooter for enhanced user interaction feat(toggle): create reusable Toggle and ToggleGroup components for consistent UI behavior

--- client/src/components/graph/GrNodeFooter.tsx
+++ client/src/components/graph/GrNodeFooter.tsx
@@ -1,12 +1,27 @@
 import {cn, type PropsWithClassName} from '@/components/shadcn/lib/utils';
-import {Tally3} from 'lucide-react';
+import {Tally1, Tally2, Tally3} from 'lucide-react';
 import type {FC} from 'react';
+import {ToggleGroup, ToggleGroupItem} from '../shadcn/ui/toggle-group';
 
 export const GrNodeFooter: FC<PropsWithClassName> = ({className}) => {
     return (
-        <div className={cn('rounded-b-md bg-gray-200/60', className)}>
-            <Tally3 className="rotate-90" />
-            <div className="px-4 py-2 truncate overflow-hidden">Footer</div>
+        <div
+            className={cn(
+                'flex justify-end rounded-b-md bg-gray-200/60',
+                className
+            )}
+        >
+            <ToggleGroup className="gap-0" type="single">
+                <ToggleGroupItem className="h-6 min-w-6 w-6" value="a">
+                    <Tally3 className="rotate-90" />
+                </ToggleGroupItem>
+                <ToggleGroupItem className="h-6 min-w-6 w-6" value="b">
+                    <Tally2 className="rotate-90" />
+                </ToggleGroupItem>
+                <ToggleGroupItem className="h-6 min-w-6 w-6" value="c">
+                    <Tally1 className="rotate-90" />
+                </ToggleGroupItem>
+            </ToggleGroup>
         </div>
     );
 };
